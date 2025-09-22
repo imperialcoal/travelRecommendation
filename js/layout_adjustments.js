edaf6a8267df16a7f934e200bf2e67a5561a3103
imperialcoal <programmer.software.dwarren@gmail.com>
2025-09-22 05:57:31 -0400

    updated html and css

--- js/layout_adjustments.js
+++ js/layout_adjustments.js
@@ -1,10 +1,16 @@
   function adjustHeroPadding() {
     const navbar = document.querySelector('.navbar');
     const hero = document.querySelector('.hero');
+    const contactHero = document.querySelector('.contact-hero');
     if (navbar && hero) {
         const navHeight = navbar.offsetHeight;
         hero.style.paddingTop = navHeight + 'px';
         hero.style.minHeight = `calc(100vh - ${navHeight}px)`;
+    }
+    else if (navbar && contactHero) {
+        const navHeight = navbar.offsetHeight;
+        contactHero.style.paddingTop = navHeight + 'px';
+        contactHero.style.minHeight = `calc(100vh - ${navHeight}px)`;
     }
 }
 
